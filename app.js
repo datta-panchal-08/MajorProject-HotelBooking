fb5a014c00c29cfee0d7d63910f65fd3f89adaf5
Dattatray Panchal <panchaldatta1508@gmail.com>
2024-04-28 16:56:33 +0530

    Create CRUD Routes By using rest api

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express =  require('express');
 const app =  express();
 const path = require('path');
+const methodOverride = require('method-override');
 const port = 3000;
 
 const mongoose =  require('mongoose');
 const Listing = require('./models/listing.js');
+const { title } = require('process');
 mongoose.connect('mongodb://127.0.0.1:27017/Wanderlust');
 
 app.set('view engine', 'ejs');
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended : true}));
+app.use(methodOverride('_method'));
 
 app.get("/",(req,res)=>{
     res.send("Welcome")
@@ -35,16 +38,45 @@ app.get("/",(req,res)=>{
     let allListings = await Listing.find({});
     res.render('./listings/index.ejs',{allListings});
   });
-  
+
+  // New Route
+app.get("/listings/new", (req, res) => {
+  res.render("./listings/new.ejs");
+});
+  // Show Route
   app.get("/listings/:id",async(req,res)=>{
        let {id} = req.params;
        let data = await Listing.findById(id);
-       
-       res.render("show.ejs",{data});
+ 
+       res.render("./listings/show.ejs",{data});
+  })
+ 
+  //Create Route
+  app.post("/listings",async(req,res)=>{
+    let newListing= new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect('/listings');
+  });
 
-       
+  //Edit Route
+  app.get("/listings/:id/edit",async(req,res)=>{
+    let {id} = req.params;
+    let data = await Listing.findById(id);
+    res.render('./listings/edit.ejs',{data})
   })
 
+  //Update Route
+  app.put("/listings/:id",async(req,res)=>{
+    let {id} =  req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+  }); 
+  //Delete Route
+  app.delete("/listings/:id",async(req,res)=>{
+    let {id} =  req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect('/listings')
+  })
 app.listen(port,()=>{
     console.log("listening on port : "+port)
 })
